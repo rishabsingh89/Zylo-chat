f5bc6cd521f4c37790a50881f54e1e566969708d
rishabsingh89 <rishabsingh8953@gmail.com>
2026-08-13 00:22:50 +0530

    Suppress IDE warning in api/index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ import os
 # Add root backend directory to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
 
-from app.main import app
+from app.main import app  # type: ignore
 
 # Vercel Serverless Function entry point
 app = app
